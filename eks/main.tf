1c8083c132dffeafe8944619c8aa6e57b877db12
kdu9303 <zheros9303@gmail.com>
2023-10-17 14:03:13 +0900

    feat: eks 테라폼 first commit

---
+++ eks/main.tf
@@ -0,0 +1,65 @@
+provider "aws" {
+  region = local.region
+  # shared_config_files=["~/.aws/config"] # Or $HOME/.aws/config
+  # shared_credentials_files = ["~/.aws/credentials"] # Or $HOME/.aws/credentials
+  profile = "dataengineer"
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    # This requires the awscli to be installed locally where Terraform is executed
+    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  }
+}
+
+data "aws_availability_zones" "available" {}
+data "aws_caller_identity" "current" {}
+
+locals {
+  name            = "greta-eks"
+  cluster_version = "1.27"
+  region          = "ap-northeast-2"
+
+  vpc_cidr = "10.1.0.0/16"
+  # azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+  azs = ["ap-northeast-2a", "ap-northeast-2b"]
+
+  tags = {
+    env  = "prod"
+    user = "dataengineer"
+  }
+}
+
+resource "aws_iam_policy" "additional" {
+  name = "${local.name}-additional"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+module "kms" {
+  source  = "terraform-aws-modules/kms/aws"
+  version = "1.1.0"
+
+  aliases               = ["eks/${local.name}"]
+  description           = "${local.name} cluster encryption key"
+  enable_default_policy = true
+  key_owners            = [data.aws_caller_identity.current.arn]
+
+  tags = local.tags
+}
