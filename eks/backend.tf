e31e3dc95ae3b0387e76d6abca6aceff195d619a
kdu9303 <zheros9303@gmail.com>
2023-11-11 15:25:23 +0900

    refactor: eks cloudwatch agent 초기설정 세팅

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,9 +1,9 @@
 terraform {
-  backend "s3" { 
-    bucket  = "greta-db-terraform-backend-tfstate"
-    key     = "eks/terraform.tfstate"
-    region  = "ap-northeast-2"
-    profile = "dataengineer"
+  backend "s3" {
+    bucket                  = "greta-db-terraform-backend-tfstate"
+    key                     = "eks/terraform.tfstate"
+    region                  = "ap-northeast-2"
+    profile                 = "dataengineer"
     shared_credentials_file = "$HOME/.aws/credentials"
   }
 }
