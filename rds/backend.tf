b1fae83173bfd258112d1e246e657f1a6c29c0a7
unknown <zheros9303@gmail.com>
2025-04-24 22:46:21 +0900

    feat: 기존 rds 폴더 삭제

--- rds/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" { 
-    bucket  = "greta-db-terraform-backend-tfstate"
-    key     = "rds/terraform.tfstate"
-    region  = "ap-northeast-2"
-    profile = "dataengineer"
-    shared_credentials_file = "$HOME/.aws/credentials"
-  }
-}
